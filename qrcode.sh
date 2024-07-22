8828be12dd7ac834551348bcfa7ecc17a1a66046
Glyn Hudson <glyn.hudson@gmail.com>
2024-07-22 19:03:12 +0000

    update

--- qrcode.sh
+++ qrcode.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
+
+echo Enter string to QR encode:
+read encodestr 
+echo Enter caption:
+read captionstr
 #qrencode -s 2 -m 5 -o qr.png $1 $2
-qrencode -s 2 -o qr.png $1 $2
+qrencode -s 2 -o qr.png $encodestr $captionstr
 #print $1
 #read -p "QR code generated press [Enter] to print or [CTRL+C] to exit"
-string=".  $2"
+string=".  $captionstr"
 convert  -pointsize 9 "label:$string" "footer.gif"
 convert -append "qr.png" "footer.gif" -gravity center  "qr-txt.png"
 convert "qr-txt.png" "qr-txt.pdf"
